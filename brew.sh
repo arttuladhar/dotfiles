32119d0be0f74082bfb312533ddafd38e9d61b6b
Aayush Tuladhar <aayush.tuladhar@gmail.com>
2019-02-13 14:53:10 -0500

    first commit

---
+++ brew.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install Bash 4.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Remove outdated versions from the cellar.
+brew cleanup
